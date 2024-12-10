df3bd130164c2691fe0b18ead30ada65290f4260
shivank04 <shivankar.rockit@gmail.com>
2024-12-10 18:04:35 +0530

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,28 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or your framework plugin
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+  server: {
+    port: 5173,
+    open: true,
+    // Add MIME type configuration
+    middlewares: [
+      (req, res, next) => {
+        if (req.url.endsWith('.webmanifest')) {
+          res.setHeader('Content-Type', 'application/manifest+json')
+        }
+        next()
       }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
     }
   },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
