59401b5050b3eb0c0a369b149680978392276f6d
shivank04 <shivankar.rockit@gmail.com>
2024-12-11 15:18:09 +0530

    fixing css issue

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    cssCodeSplit: false, // This ensures CSS is bundled into one file
+    cssMinify: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/styles/[name]-[hash][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
-        }
+        manualChunks: undefined
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
     }
   }
 })
