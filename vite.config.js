6cb13bc89cfce7687ca35a072b43ba73b30443f1
shivank04 <shivankar.rockit@gmail.com>
2024-12-11 14:50:17 +0530

    adding vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or your framework plugin
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
+    sourcemap: true
   }
 })
