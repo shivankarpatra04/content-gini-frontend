c03470e9e6667ee1bdc274e70af8e326b0a80461
shivank04 <shivankar.rockit@gmail.com>
2024-12-11 15:39:47 +0530

    fixing css issue

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,15 @@ export default defineConfig({
     cssMinify: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
       }
     }
   }
